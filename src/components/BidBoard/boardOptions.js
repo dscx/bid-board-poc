bb5203c55c4fe4ae947120782eaefae643fcc474
dscx <dnchmbrln@gmail.com>
2019-03-26 08:10:41 -0700

    add data

---
+++ src/components/BidBoard/boardOptions.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    title: "Total estimators on your team",
+    min: 1,
+    max: 100,
+    key: "totalEstimators",
+    step: 1,
+  },
+  {
+    title: "Average hours spent to complete a bid",
+    min: 0.5,
+    max: 24,
+    key: "hoursSpent",
+    step: 0.5,
+  },
+  {
+    title: "Total missed bids per year",
+    min: 1,
+    max: 30,
+    key: "missedBids",
+    step: 1,
+  },
+  {
+    title: "Average bid value",
+    min: 1000,
+    max: 1000000,
+    key: "averageBidValue",
+    step: 1000,
+  },
+  {
+    title: "Hit rate on bids submitted",
+    min: 1,
+    max: 20,
+    key: "hitRate",
+    step: 1,
+  },
+]
