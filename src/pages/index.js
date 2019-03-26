a3f459e2c20d27955ebb9b5cfcc36b617181a3fa
dscx <dnchmbrln@gmail.com>
2019-03-26 08:11:44 -0700

    update main page with new components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,76 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Controls from "../components/BidBoard/controls"
+import Summary from "../components/BidBoard/summary"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  state = {
+    totalEstimators: 1,
+    hoursSpent: 0.5,
+    missedBids: 1,
+    averageBidValue: 1000,
+    hitRate: 1,
+  }
+
+  onUpdate = ({ value, key }) => {
+    this.setState({ [key]: value })
+  }
+
+  getPotentialGrowth = () => {
+    const {
+      averageBidValue,
+      totalEstimators,
+      hoursSpent,
+      missedBids,
+      hitRate,
+    } = this.state
+
+    return (
+      (totalEstimators * hoursSpent * missedBids * averageBidValue) / hitRate
+    )
+  }
+
+  render() {
+    const {
+      averageBidValue,
+      totalEstimators,
+      hoursSpent,
+      missedBids,
+      hitRate,
+    } = this.state
+
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`roi`, `bid board`, `investment`]} />
+        <h1>How much can you grow with Bid Board? Check it out. </h1>
+        <div
+          style={{
+            maxWidth: `950px`,
+            marginBottom: `1.45rem`,
+            boxShadow: "-2px 2px 10px 5px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <Controls
+            onUpdate={this.onUpdate}
+            /**
+             * this is causing the entire index page to re-render, along with all its children.
+             * how should we pass this data to summary then?
+             * we couod move summary into the controls thing and try it
+             */
+          />
+          <Summary
+            totalEstimators={totalEstimators}
+            hoursSpent={hoursSpent}
+            missedBids={missedBids}
+            averageBidValue={averageBidValue}
+            hitRate={hitRate}
+            potentialGrowth={this.getPotentialGrowth()}
+          />
+        </div>
+      </Layout>
+    )
+  }
+}
 
 export default IndexPage
